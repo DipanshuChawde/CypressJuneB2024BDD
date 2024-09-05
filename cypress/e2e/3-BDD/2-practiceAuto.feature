8f2456486b0c494171f69ada67b981736e3f14f7
DipanshuChawde <dipanshuchoude@gmail.com>
2024-09-05 09:36:38 +0530

    5sept

--- cypress/e2e/3-BDD/2-practiceAuto.feature
+++ cypress/e2e/3-BDD/2-practiceAuto.feature
@@ -1,7 +1,36 @@
 Feature: i want to test automatiopn practice login page
 
-    Scenario: verify login page for valid data
+    Background: navigate to web page
         Given i navigate to login page
+
+    # Scenario 1
+    Scenario: verify login page for valid data
+        # Given i navigate to login page
         When i enter login uid and pw
         And i clickon login button
         Then i verify login page
+
+    # Scenario 2
+    Scenario Outline: verify login page for multiple data
+        # Given i navigate to login page
+        When i enter login <useremail> and <pw>
+        And i clickon login button
+        Then i verify login page <useremail>
+
+        Examples:
+            | useremail              | pw           |
+            | stageTesting@gmail.com | dipanshu@123 |
+            | dipanshu33@gmail.com   | dipanshu@123 |
+
+    # Scenario 3
+
+    Scenario Outline: verify login page for multiple data
+        # Given i navigate to login page
+        When i enter login1 "<useremail>" and "<pw>"
+        And i clickon login button
+        Then i verify login page1 "<useremail>"
+
+        Examples:
+            | useremail              | pw           |
+            | stageTesting@gmail.com | dipanshu@123 |
+            | dipanshu33@gmail.com   | dipanshu@123 |
--- cypress/e2e/3-BDD/2-practiceAuto/practiceAuto.cy.js
+++ cypress/e2e/3-BDD/2-practiceAuto/practiceAuto.cy.js
@@ -1,30 +1,54 @@
 ///<reference types='cypress' />
-import {Given, When, Then, And} from "cypress-cucumber-preprocessor/steps";
+import { Given, When, Then, And } from "cypress-cucumber-preprocessor/steps";
 
-Given('i navigate to login page',function(){
-
+Given('i navigate to login page', function () {
+    cy.visit('https://practice.automationtesting.in/my-account/')
 })
 
 
-When('i enter login uid and pw',function(){
+When('i enter login uid and pw', function () {
+    cy.get('[id="username"]').type('dipanshu33@gmail.com')
+    cy.get('[id="password"]').type('dipanshu@123')
+})
+
+And('i clickon login button', function () {
+    cy.get('[name="login"]').click()
+})
+
+Then('i verify login page', function () {
+
+    let email = 'dipanshu33@gmail.com'
+    let uid = email.split("@")
+    cy.get('strong').should('have.text', uid[0])
 
 })
 
-And('i clickon login button',function(){
-    
+//----------Scenario 2-------------------
+
+When('i enter login {word} and {word}', (uid, pw) => {
+    cy.get('[id="username"]').type(uid)
+    cy.get('[id="password"]').type(pw)
+
 })
 
-Then('i verify login page',function(){
+Then('i verify login page {word}', (em) => {
 
+    let email = em
+    let uid = email.split("@")
+    cy.get('strong').should('have.text', uid[0])
 })
 
 
+//----------Scenario 3-------------------
+When('i enter login1 {string} and {string}', (uid, pw) => {
+    cy.get('[id="username"]').type(uid)
+    cy.get('[id="password"]').type(pw)
 
-// uid:'productionTesting@gmail.com',
-// pw :'dipanshu@1'
-// uid:'stageTesting@gmail.com',
-// pw :'dipanshu@123'
+})
 
-// dipanshu33@gmail.com
+Then('i verify login page1 {string}', (em) => {
 
-// dipanshu@123
+    let email = em
+    let uid = email.split("@")
+    cy.get('strong').should('have.text', uid[0])
+})
